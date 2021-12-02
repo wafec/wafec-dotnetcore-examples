84d786c468b84029d7325942945282324093f569
Wallace <wallacefcardoso@gmail.com>
2021-12-02 09:57:34 -0300

    exception handler as middleware

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # wafec-dotnetcore-examples
+
+Solution001 = loading controllers from different assemblies
+Solution002 = configure DI in another assembly and load it from the main project
+Solution003 = setting up automapper to load from assemblies
+Solution004 = create a middleware to compose error messages when exceptions happen
---
+++ Solution004_CreateMiddlewareForException/WebApi/Controllers/FaultyController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class FaultyController
+    {
+        [HttpGet]
+        public IActionResult Argument()
+        {
+            throw new ArgumentException();
+        }
+
+        [HttpGet]
+        public IActionResult Null()
+        {
+            throw new NullReferenceException();
+        }
+    }
+}
